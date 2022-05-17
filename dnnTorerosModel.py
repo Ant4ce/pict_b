d23ea36faa26cc80c2a321c87489c9aa53bd12c2
wessel_weernink <wessel.weernink@code.berlin>
2022-05-17 21:27:44 +0200

    backup

--- dnnTorerosModel.py
+++ dnnTorerosModel.py
@@ -12,12 +12,16 @@ def set_seeds(seed = 100):
     tf.random.set_seed(seed)
     
 def cw(df):
-    c0, c1 = np.bincount(df["dir"])
-    w0 = (1/c0) * (len(df)) / 2
-    w1 = (1/c1) * (len(df)) / 2
-    return {0:w0, 1:w1}
+    c0, c1, c2, c3, c4 = np.bincount(df["PredictionGroup"])
+    w0 = (1/c0) * (len(df)) / 5
+    w1 = (1/c1) * (len(df)) / 5
+    w2 = (1/c2) * (len(df)) / 5
+    w3 = (1/c3) * (len(df)) / 5
+    w4 = (1/c4) * (len(df)) / 5
 
-optimizer = Adam(lr = 0.0001)
+    return {0:w0, 1:w1, 2:w2, 3:w3, 4:w4 }
+
+optimizer = Adam(lr = 0.00003)
 
 def create_model(hl = 2, hu = 100, dropout = False, rate = 0.3, regularize = False,
                  reg = l1(0.0005), optimizer = optimizer, input_dim = None):
@@ -31,6 +35,8 @@ def create_model(hl = 2, hu = 100, dropout = False, rate = 0.3, regularize = Fal
         model.add(Dense(hu, activation = "relu", activity_regularizer = reg))
         if dropout:
             model.add(Dropout(rate, seed = 100))
-    model.add(Dense(1, activation = "sigmoid"))
-    model.compile(loss = "binary_crossentropy", optimizer = optimizer, metrics = ["accuracy"])
+
+    #model.add(Dense(1, activation = "sigmoid"))
+    model.add(Dense(5, activation = "softmax"))
+    model.compile(loss = "sparse_categorical_crossentropy", optimizer = optimizer, metrics = ["accuracy"])
     return model
